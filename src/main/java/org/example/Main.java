b0b7a37dde6e223cf58890f18abf71ef7a5ddd0b
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-09-20 21:05:18 -0300

    strategy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,25 @@
 package org.example;
 
 import org.example.impostos.CalculadoraDeImpostos;
-import org.example.impostos.TipoImposto;
+import org.example.impostos.ICMS;
+import org.example.impostos.ISS;
+import org.example.impostos.Imposto;
 import org.example.orcamento.Orcamento;
-import org.w3c.dom.ls.LSOutput;
 
-import javax.crypto.spec.PSource;
 import java.math.BigDecimal;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
 
-        System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ICMS));
-        System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ISS));
-        System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ILUSTRAR));
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS()));
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
+//        System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ILUSTRAR));
     }
-
 
 
 }
---
+++ src/main/java/org/example/impostos/Imposto.java
@@ -0,0 +1,10 @@
+package org.example.impostos;
+
+import org.example.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public interface Imposto {
+
+    public BigDecimal calcular(Orcamento orcamento);
+}
