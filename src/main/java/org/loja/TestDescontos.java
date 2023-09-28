72b9feb66edae8ebc8ea0922ccad952088adf05a
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-09-28 19:20:06 -0300

    composite

--- src/main/java/org/loja/TestDescontos.java
+++ src/main/java/org/loja/TestDescontos.java
@@ -10,18 +10,14 @@ public class TestDescontos {
     public static void main(String[] args) {
         CalculadoraDeDesconto calculadoraDeDesconto = new CalculadoraDeDesconto();
 
-        Orcamento a = new Orcamento(new BigDecimal("200"), 6);
-        Orcamento b = new Orcamento(new BigDecimal("100"), 4);
-        Orcamento c = new Orcamento(new BigDecimal("1000"), 1);
+        Orcamento primeiro = new Orcamento(new BigDecimal("100"));
+        Orcamento segundo = new Orcamento(new BigDecimal("200"));
+        Orcamento terceiro = new Orcamento(new BigDecimal("1000"));
 
 
-//        System.out.println("valor desconto: " + calculadoraDeDesconto.calcularDesconto(new Orcamento(new BigDecimal("100"), 6)));
-//        System.out.println("valor desconto: " + calculadoraDeDesconto.calcularDesconto(new Orcamento(new BigDecimal("100"), 4)));
-//        System.out.println("valor desconto: " + calculadoraDeDesconto.calcularDesconto(new Orcamento(new BigDecimal("1000"), 1)));
-
-        System.out.println(calculadoraDeDesconto.calcularDesconto(a));
-        System.out.println(calculadoraDeDesconto.calcularDesconto(b));
-        System.out.println(calculadoraDeDesconto.calcularDesconto(c));
+        System.out.println(calculadoraDeDesconto.calcularDesconto(primeiro));
+        System.out.println(calculadoraDeDesconto.calcularDesconto(segundo));
+        System.out.println(calculadoraDeDesconto.calcularDesconto(terceiro));
 
     }
 }
